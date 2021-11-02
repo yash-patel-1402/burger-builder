5f621cbe8c3190041f01f335451962fe2f687e7f
yash-patel-1402 <yash.patel.redcode@gmail.com>
2021-11-02 06:35:27 +0530

    deployment update

--- src/index.js
+++ src/index.js
@@ -12,9 +12,10 @@ import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 
-const composeEnhancers = process.env.NODE_ENV
-  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-  : null || compose;
+const composeEnhancers =
+  (process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null) || compose;
 const store = createStore(
   combineReducers({
     burger: burgerReducer,
